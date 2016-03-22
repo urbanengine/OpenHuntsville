1b63f930f667a2792c0174289785e98edb99af87
Kyle Newman <kyle@skylenewman.com>
2016-03-21 23:37:06 -0500

    test

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -71,7 +71,7 @@ Pakyow::App.routes do
       end
     }
     pp subset
-    view.scope(:people).apply(subset)
+    view.scope(:people).apply(unapproved)
     view.scope(:head).apply(request)
     view.scope(:main_menu).apply(request)
   end
