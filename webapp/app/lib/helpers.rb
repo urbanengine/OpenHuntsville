d878f09fbe82fa78a4b7b170b7a62d1979337a3d
Kyle Newman <kyle@skylenewman.com>
2016-07-24 10:15:45 -0500

    Removed duplicate app directory

--- webapp/app/lib/helpers.rb
+++
@@ -1,3 +0,0 @@
-module Pakyow::Helpers
-  # define methods here that are available from routes, bindings, etc
-end
--- webapp/app/lib/routes.rb
+++
@@ -1,7 +0,0 @@
-Pakyow::App.routes do
-  default do
-    logger.info 'hello'
-  end
-
-  # define application routes here
-end
