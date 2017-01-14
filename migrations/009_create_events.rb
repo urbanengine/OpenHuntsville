2ed836c95b305d6beb17c80aad3edda27c87cca3
Kyle Newman <kyle@skylenewman.com>
2017-01-13 23:16:25 -0600

    Added migrations

---
+++ migrations/009_create_events.rb
@@ -0,0 +1,21 @@
+Sequel.migration do
+  up do
+    create_table :events do
+      primary_key   :id
+      DateTime      :created_at
+      DateTime      :updated_at
+      String        :name
+      String        :description
+      DateTime      :start_datetime
+      Integer       :duration
+      Boolean       :approved
+      foreign_key   :parent_id, :events, :null=>true
+      foreign_key   :group_id,  :groups, :null=>false
+      foreign_key   :venue_id,  :venues, :null=>false
+    end
+  end
+
+  down do
+    drop_table :events
+  end
+end
