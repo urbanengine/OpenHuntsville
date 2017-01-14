426ae6d717d25467133d0fa1999e1e21dca821a3
David Jones <davidhjones89@gmail.com>
2017-01-14 17:54:48 -0600

    test
    test

--- app/lib/routes/events.rb
+++ app/lib/routes/events.rb
@@ -18,8 +18,10 @@ Pakyow::App.routes(:events) do
           events_all = Event.where('start_datetime > ?', DateTime.now).all
           events_all.each { |event|
             puts "manage event.start_datetime"
+            puts event.start_datetime.to_datetime
             puts event.start_datetime
-            event.start_datetime = event.start_datetime.to_datetime.change(:offset => '-06:00')
+            event.start_datetime = event.start_datetime.change(:offset => '-06:00')
+            puts event.start_datetime.to_datetime
             puts event.start_datetime
             puts ""
           }
