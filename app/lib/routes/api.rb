8c1bec56cfc8e5f24e52526576ef70db7964a773
David Jones <davidhjones89@gmail.com>
2017-03-04 11:17:40 -0600

    Expose api endpoint to retrieve a specific cwn_flyer
    Expose api endpoint to retrieve a specific cwn_flyer

--- app/lib/routes/api.rb
+++ app/lib/routes/api.rb
@@ -98,6 +98,61 @@ Pakyow::App.routes(:api) do
             }
             response.write(']')
           end # get 'cwn_flyer'
+
+          get 'cwn_flyer/:cwn_instance_number' do
+            # "approved":true,
+            # "cwn":88,
+            # "timestamp":"2017-01-09T20:27:22.161Z",
+            # "group":"Designer's Corner",
+            # "title":"CoWorking Night Tshirt Competition",
+            # "description":"Come in to get some last minute ideas for your entry to the CoWorking Night Tshirt competition! ",
+            # "date":"2017-01-11T06:00:00.000Z",
+            # "time_req_form":"8:00 PM",
+            # "time_req":"1899-12-31T01:48:52.000Z",
+            # "room_req":"Milky Way Row",
+            # "start_time":"2017-01-12T02:00:00.000Z",
+            # "end_time":"2017-01-12T03:00:00.000Z",
+            # "category":"Programming",
+            # "icon":"terminal"
+
+            #For now, we'll keep this only exposed for cwn
+            cwn = Group.where("name = 'CoWorking Night'").first
+            if cwn.nil?
+              redirect '/errors/403'
+            end
+
+            #Now lets get all the events for this group. This means all of this group's events and its event's children
+            cwn_event = Event.where("approved = true AND start_datetime > ? AND group_id = ? AND instance_number = ?", DateTime.now.utc, cwn.id, params[:cwn_instance_number]).order(:start_datetime).first
+            events = get_child_events_for_event(cwn_event)
+            response.write('[')
+            first_time = true
+            events.each { |event|
+              if first_time == true
+                first_time = false
+              else
+                response.write(',')
+              end
+              json =
+                {
+                  "approved" => event.approved,
+                  "cwn" => cwn_event.instance_number,
+                  "timestamp" => event.created_at.utc,
+                  "group" => Group.where("id = ?", event.group_id).first.name,
+                  "title" => event.name,
+                  "description" => event.summary,
+                  "date" => event.start_datetime.utc,
+                  "time_req_form" => event.start_datetime.utc,
+                  "time_req" => event.start_datetime.utc,
+                  "room_req" => Venue.where("id = ?", event.venue_id).first.name,
+                  "start_time" => event.start_datetime.utc,
+                  "end_time" => (event.start_datetime.to_time + event.duration.hours).utc,
+                  "category" => event.flyer_category,
+                  "icon" => event.flyer_fa_icon
+                }
+                response.write(json.to_json)
+            }
+            response.write(']')
+          end #get 'cwn_flyer/:cwn_instance_num'
         end # collection do
       end # expand :restful, :v1, '/v1' do
 
